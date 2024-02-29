6ace58a03853debe99a1f289052d0e9dba73375d
Dan Vanderkam <danvdk@gmail.com>
2024-02-29 09:33:21 -0500

    transposed blocks

--- cryptopals/challenge6.py
+++ cryptopals/challenge6.py
@@ -2,6 +2,8 @@
 
 import sys
 
+from cryptopals.challenge3 import bits_to_bytes
+
 
 def edit_distance(bits1, bits2):
     assert len(bits1) == len(bits2)
@@ -40,10 +42,47 @@ def base64_to_bits(str):
     return bits
 
 
+def chunk(xs: list, n: int):
+    # split an array into chunks of length n; may truncate last one
+    for i in range(0, len(xs), n):
+        if i + n > len(xs):
+            break
+        yield xs[i:i+n]
+
+
 if __name__ == '__main__':
     bits = [
         bit
         for line in open(sys.argv[1]).readlines()
         for bit in base64_to_bits(line.strip())
     ]
-    print(len(bits))
+    print(len(bits), 'bits')
+
+    # best keysizes:
+    # 5: (1.2, ks=5, ed=6)
+    # 3: (2.0, ks=3, ed=6)
+    # 2: (2.5, ks=2, ed=5)
+    scores = []
+    for keysize in range(2, 41):
+        keysize_bits = 8 * keysize
+        first = bits[0:keysize_bits]
+        second = bits[keysize_bits:2*keysize_bits]
+        assert len(first) == keysize_bits
+        assert len(second) == keysize_bits
+        distance = edit_distance(first, second)
+        norm_d = distance / keysize
+        scores.append((norm_d, keysize, distance))
+    scores.sort()
+    print(scores[:3])
+    keysize = scores[0][1]
+    print(f'{keysize=}')
+
+    u8s = bits_to_bytes(bits)
+    blocks = [*chunk(u8s, keysize)]
+    print(f'n blocks: {len(blocks)}')
+    print(len(blocks[0]))
+    print(len(blocks[-1]))
+
+    trans = [*zip(*blocks)]
+    print(f'n trans={len(trans)}')
+    print(f'n trans[0]={len(trans[0])}')
